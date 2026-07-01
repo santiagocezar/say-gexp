246bb5a3259d1fe8827b011bb9bebcb67d4df823
Santiago Cézar <santiagocezar2013@gmail.com>
2026-07-01 00:50:28 -0300

    await in repl

--- repl.ts
+++ repl.ts
@@ -1,4 +1,6 @@
+import { Session } from "node:inspector/promises";
 import readline from "node:readline/promises";
+import vm from "node:vm";
 import { stdin as input, stdout as output } from "node:process";
 import { Reader } from "./src/core/reader.ts";
 import { print } from "esrap";
@@ -15,13 +17,18 @@ function _PLUS_(a, b) {
 }
 
 const reader = new Reader();
+const session = new Session();
+const context = vm.createContext(undefined, {
+    importModuleDynamically: vm.constants.USE_MAIN_CONTEXT_DEFAULT_LOADER,
+});
+
+session.connect();
+
 let pending = false;
 while (true) {
     const prog = await rl.question(pending ? "... " : ">>> ");
 
     for (const token of Reader.tokens(prog)) {
-        console.log(token);
-
         const { done, forms } = reader.push(token);
         pending = !done;
 
@@ -37,12 +44,26 @@ while (true) {
                 }
                 console.dir(ast, { depth: null });
 
-                const code = print(ast, ts()).code;
+                const code = print(ast as any, ts()).code;
 
                 console.log(code);
                 try {
-                    const result = eval(code);
-                    console.log(result);
+                    const { result, exceptionDetails } = await session.post(
+                        "Runtime.evaluate",
+                        {
+                            // why is it 2?, i don't know how to get the id
+                            // for the context created above, but this seems
+                            // to be it?
+                            contextId: 2,
+                            awaitPromise: true,
+                            replMode: true,
+                            expression: code,
+                        },
+                    );
+                    if (exceptionDetails) {
+                        console.error(exceptionDetails);
+                    }
+                    console.log(result.value);
                 } catch (err) {
                     console.error(err);
                     continue;
--- src/eval.ts
+++ src/eval.ts
@@ -1,6 +1,6 @@
-import { asExpression, asLiteral } from "./ast.ts";
+import { astProgram } from "./ast/statements.ts";
 import { type Form } from "./core/reader.ts";
 
 export function transpileToAST(form: Form) {
-    return asExpression(form);
+    return astProgram(form);
 }
